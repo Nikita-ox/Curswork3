5a32191c33843a5c98688b2e89884120b36c0f16
NIK <nikita1z@mail.ru>
2022-07-04 19:17:10 +0300

    commit
    Signed-off-by: NIK <nikita1z@mail.ru>

---
+++ curswork/project/views/genres.py
@@ -0,0 +1,31 @@
+from project.container import genre_service
+from flask_restx import Namespace, Resource, abort
+from project.exceptions import GenreNotFound
+from flask import request
+
+
+genres_ns = Namespace("genres")
+
+
+@genres_ns.route("/")
+class GenresView(Resource):
+    @genres_ns.response(200, "OK")
+    def get(self):
+        try:
+            page = request.args.get("page", type=int)
+            filter = {"page": page}
+            return genre_service.get_all_genres(filter)
+        except GenreNotFound:
+            abort(404, message="Genres not found")
+
+
+@genres_ns.route("/<int:genre_id>")
+class GenreView(Resource):
+    @genres_ns.response(200, "OK")
+    @genres_ns.response(404, "Genre not found")
+    def get(self, genre_id: int):
+        """Get genre by id"""
+        try:
+            return genre_service.get_item_by_id(genre_id)
+        except GenreNotFound:
+            abort(404, message="Genre not found")
